210840d41c3e76e466ba7b6cf497bbf02101e583
Dénes Temleitner <110195640+temdenes@users.noreply.github.com>
2024-06-12 16:32:33 +0200

    End Apple Dev Turorials this part: Collection views and navigation Displaying reminder details

--- Today/AppDelegate.swift
+++ Today/AppDelegate.swift
@@ -14,6 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UINavigationBar.appearance().tintColor = .todayPrimaryTint
+        UINavigationBar.appearance().backgroundColor = .todayNavigationBackground
+        let navBarAppearence = UINavigationBarAppearance()
+        navBarAppearence.configureWithOpaqueBackground()
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearence
         return true
     }
 
